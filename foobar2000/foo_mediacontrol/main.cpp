b1e331aa37866936d9cbb08b98f8cafad5506e72
yhdsl <56674878+yhdsl@users.noreply.github.com>
2026-03-11 17:42:35 +0800

    更新版本号

--- foobar2000/foo_mediacontrol/main.cpp
+++ foobar2000/foo_mediacontrol/main.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 
-DECLARE_COMPONENT_VERSION("Windows 10 媒体控制支持","1.4","为 foobar2000 添加 Windows 10 媒体控制功能支持。");
+DECLARE_COMPONENT_VERSION("Windows 10 媒体控制支持","1.4.1","为 foobar2000 添加 Windows 10 媒体控制功能支持。");
 VALIDATE_COMPONENT_FILENAME("foo_mediacontrol.dll");
